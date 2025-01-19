898328c13d50338aa84565cd7e55ab569d249425
Leonardo Cunha <agile.ice4816@fastmail.com>
2025-01-19 08:05:39 -0300

    feat: update help info

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -11,6 +11,14 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for the sake of learning\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2025\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
+
 	task := flag.String("task", "", "Task to be included in the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
